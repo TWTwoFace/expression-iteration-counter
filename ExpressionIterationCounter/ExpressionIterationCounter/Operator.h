c0c9ed0686acf1517937976a4a316b98e41cccd9
TWTwoFace <pipo2017@mail.ru>
2025-06-21 02:50:20 +0300

    feat: add operator enum class

---
+++ ExpressionIterationCounter/ExpressionIterationCounter/Operator.h
@@ -0,0 +1,27 @@
+#pragma once
+enum class Operator
+{
+	Add,
+	Substract,
+	Multiply,
+	Divide,
+	Modulo,
+	More,
+	Less,
+	MoreOrEquals,
+	LessOrEquals,
+	Equals,
+	Unequals,
+	If,
+	Not,
+	And,
+	Or,
+	BitwiseAnd,
+	BitwiseOr,
+	BitwiseXor,
+	Inversion,
+	BitwiseLeftShift,
+	BitwiseRightShift,
+	TakingByIndex,
+	Ternary
+};
