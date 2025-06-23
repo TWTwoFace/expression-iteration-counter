f2e8db9c290d43f382b3bb6ac137f04999ca0157
TWTwoFace <pipo2017@mail.ru>
2025-06-23 03:07:00 +0300

    feat: add read file function

--- ExpressionIterationCounter/ExpressionIterationCounter/main.cpp
+++ ExpressionIterationCounter/ExpressionIterationCounter/main.cpp
@@ -6,6 +6,30 @@
 #define SLASH "\\"
 #define PROGRAM_NAME "Expression Iteration Counter"
 
+bool ReadFile(const std::string path, std::vector<std::string> &fileData, ErrorLogger& logger)
+{
+	std::fstream file;
+	file.open(path);
+
+	if (!file.is_open())
+	{
+		Error error(ErrorType::FileDoesNotExists, "Не удается открыть файл");
+		logger.LogError(error);
+		return false;
+	}
+
+	std::string line;
+
+	while (std::getline(file, line))
+	{
+		fileData.push_back(line);
+	}
+
+	file.close();
+
+	return true;
+}
+
 int main(int argc, char* argv[])
 {
 	if (argc != 4)
