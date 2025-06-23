90e3b21255c2c6df0497ff4d7ca9dd38d32e85bf
TWTwoFace <pipo2017@mail.ru>
2025-06-23 03:22:59 +0300

    refactor: moved utils functions into module 'utils'

---
+++ ExpressionIterationCounter/ExpressionIterationCounter/Utils.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <vector>
+#include <string>
+
+std::vector<std::string> SplitString(std::string str, const char* delimeter);
+
+bool IsOperator(std::string token);
+
+bool IsConstant(std::string str);
+
+bool IsType(std::string str);
+
+bool IsKeyword(std::string str);
+
+bool IsValidVariableName(std::string str);
--- ExpressionIterationCounter/ExpressionIterationCounter/Validators.cpp
+++ ExpressionIterationCounter/ExpressionIterationCounter/Validators.cpp
@@ -3,80 +3,10 @@
 #include <cstring>
 #include <stdexcept>
 #include <cmath>
+#include "Utils.h"
 
 #pragma warning(disable: 4996)
 
-std::vector<std::string> SplitString(std::string str, const char* delimeter)
-{
-	std::vector<std::string> result;
-	char* token = std::strtok((char*)str.c_str(), delimeter);
-
-	while (token != nullptr) {
-		result.emplace_back(token);
-		token = std::strtok(nullptr, delimeter);
-	}
-
-	return result;
-}
-
-bool IsOperator(std::string token)
-{
-	std::vector<std::string> operators =
-	{
-		"+", "-", "*", "/",
-		"%", ">", "<", ">=",
-		"<=", "==", "!=", "!",
-		"&&", "||", "&", "|",
-		"^", "~", "<<", ">>",
-		"[]",
-	};
-
-	return std::find(operators.begin(), operators.end(), token) != operators.end();
-}
-
-bool IsConstant(std::string str)
-{
-	return str == "true" || str == "false";
-}
-
-bool IsType(std::string str)
-{
-	std::vector<std::string> types =
-	{
-		"bool", "short", "int", "long",
-		"float", "double", "char", "char[]",
-		"short[]", "int[]", "long[]", "float[]",
-		"double[]"
-	};
-	return std::find(types.begin(), types.end(), str) != types.end();
-}
-
-bool IsKeyword(std::string str)
-{
-	std::vector<std::string> keywords =
-	{
-		"do", "while", "for", "if", "else", "void", "return"
-	};
-
-	return IsType(str) || IsOperator(str) || std::find(keywords.begin(), keywords.end(), str) != keywords.end();
-}
-
-bool IsValidVariableName(std::string str)
-{
-	if (str.size() == 0)
-		return false;
-
-	if (isdigit(str[0]))
-		return false;
-
-	for (int i = 0; i < str.size(); i++)
-	{
-		if ((str[i] < 65 || str[i] > 122) && !isdigit(str[i]) && str[i] != '_')
-			return false;
-	}
-
-	return true;
-}
 
 bool ValidateTreeFile(const std::vector<std::string>& fileData, ErrorLogger& logger)
 {
