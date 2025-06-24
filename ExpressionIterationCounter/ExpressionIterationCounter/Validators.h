0b0f5b73f7c8464d7bc86cfe1746b4898e5cc63c
TWTwoFace <pipo2017@mail.ru>
2025-06-24 07:03:26 +0300

    doc: add comments into validators.h for doxygen

--- ExpressionIterationCounter/ExpressionIterationCounter/Validators.h
+++ ExpressionIterationCounter/ExpressionIterationCounter/Validators.h
@@ -5,8 +5,22 @@
 #include "Logger.h"
 #include "Operator.h"
 
+/*! Функция, валидирующая данные из файла разбора выражения
+	\param[in] fileData - вектор строк из файла
+	\param[in,out] logger - логгер, для хранения возможных ошибок
+*/
 bool ValidateTreeFile(const std::vector<std::string>& fileData, ErrorLogger& logger);
 
+/*! Функция, валидирующая данные из файла типов переменных
+	\param[in] fileData - вектор строк из файла
+	\param[in] variables - вектор имен переменных, содержащихся в дереве
+	\param[in,out] logger - логгер, для хранения возможных ошибок
+*/
 bool ValidateTypesFile(const std::vector<std::string>& fileData, const std::set<std::string>& variables, ErrorLogger& logger);
 
+/*! Функция, валидирующая данные из файла операторов
+	\param[in] fileData - вектор строк из файла
+	\param[in] operators - вектор операторов, содержащихся в дереве
+	\param[in,out] logger - логгер, для хранения возможных ошибок
+*/
 bool ValidateIterationsFile(const std::vector<std::string>& fileData, const std::set<Operator>& operators, ErrorLogger& logger);
