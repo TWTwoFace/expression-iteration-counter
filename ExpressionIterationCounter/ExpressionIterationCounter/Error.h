3b0134a2033b762d2abff21ea18c769ef7ce7036
TWTwoFace <pipo2017@mail.ru>
2025-06-24 06:11:22 +0300

    doc: add comments into error for doxygen

--- ExpressionIterationCounter/ExpressionIterationCounter/Error.h
+++ ExpressionIterationCounter/ExpressionIterationCounter/Error.h
@@ -2,16 +2,28 @@
 #include <string>
 #include "ErrorType.h"
 
+//!Класс, представляющий ошибку
 class Error
 {
 private:
-	ErrorType m_type;
-	std::string m_description;
-	std::string m_fileTrace;
+	ErrorType m_type;			//!<Тип ошибки
+	std::string m_description;	//!<Описание ошибки
+	std::string m_fileTrace;	//!<Трейс ошибки
 
 public:
+	/*! Конструктор
+	    \param[in] type - тип ошибки
+		\param[in] description - описание ошибки
+		\param[in] fileTrace - трейсОшибки 
+	*/
 	Error(ErrorType type, std::string description, std::string fileTrace = "");
+	/*! Получить тип ошибки
+		\return - тип ошибки
+	*/
 	ErrorType GetType();
+	/*! Получить сообщение ошибки
+		\return - строка, содержащая преобразованный текст ошибки для вывода
+	*/
 	std::string GetMessage();
 };
 
