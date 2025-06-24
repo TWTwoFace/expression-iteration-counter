8c35af19d4eac8a637e02a29b4c06b4955b770a9
TWTwoFace <pipo2017@mail.ru>
2025-06-24 06:45:46 +0300

    doc: add comments into logger.h for doxygen

--- ExpressionIterationCounter/ExpressionIterationCounter/Logger.h
+++ ExpressionIterationCounter/ExpressionIterationCounter/Logger.h
@@ -2,14 +2,26 @@
 #include <vector>
 #include "Error.h"
 
+//! Класс, представляющий логгер
 class ErrorLogger
 {
 private:
-	std::vector<Error> m_errors;
+	std::vector<Error> m_errors;		//!<Добавленные ошибки
 
 public:
+	/*! Добавить ошибку
+		\param[in] error - ошибка
+	*/
 	void LogError(Error &error);
+
+	/*! Получить ошибки
+		\return ошибки, добавленные ранее
+	*/
 	std::vector<Error>* GetErrors();
+
+	/*! Пустой ли ErrorLogger
+		\return true - если ошибок нет, false - в ином случае
+	*/
 	bool IsEmpty();
 };
 
