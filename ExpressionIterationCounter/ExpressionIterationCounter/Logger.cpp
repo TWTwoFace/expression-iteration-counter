8a5fcb0b8bc82c6eafeb0aaec3b592d7355937b2
TWTwoFace <pipo2017@mail.ru>
2025-06-24 05:06:48 +0300

    style: add comments into all logger methods

--- ExpressionIterationCounter/ExpressionIterationCounter/Logger.cpp
+++ ExpressionIterationCounter/ExpressionIterationCounter/Logger.cpp
@@ -2,15 +2,18 @@
 
 void ErrorLogger::LogError(Error& error)
 {
+	// Добавими ошибку в конец вектора, хранящего ошибки
 	m_errors.push_back(error);
 }
 
 std::vector<Error>* ErrorLogger::GetErrors()
 {
+	// Вернем указатель на вектор, храняший ошибки
 	return &m_errors;
 }
 
 bool ErrorLogger::IsEmpty()
 {
+	// Вернем true, если размер вектора с ошибка равен нулю, иначе false
 	return m_errors.size() == 0;
 }
